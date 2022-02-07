0732a047e0f42994d598855baf20f89defb5350d
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-02-07 14:30:43 +0100

    Update License

--- source/apps/portals/views/entities/forums/thread/delete_.d
+++ source/apps/portals/views/entities/forums/thread/delete_.d
@@ -1,2 +1,7 @@
+/***********************************************************************************************************************
+*	Copyright: © 2017-2022 UI Manufaktur UG / 2022 Ozan Nurettin Süel (sicherheitsschmiede)                              *
+*	License: Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt]                                       *
+*	Authors: UI Manufaktur UG Team, Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
+***********************************************************************************************************************/
 module apps.portals.views.entities.forums.thread.delete_;
 
